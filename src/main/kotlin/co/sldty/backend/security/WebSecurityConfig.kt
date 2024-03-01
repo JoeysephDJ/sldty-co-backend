be20dd847cbdba8bcad694425c276b2bcca3a8d6
Joseph Johnson <joeysephdj@gmail.com>
2024-03-01 01:08:15 -0500

    Initialize Spring Boot project

---
+++ src/main/kotlin/co/sldty/backend/security/WebSecurityConfig.kt
@@ -0,0 +1,33 @@
+package co.sldty.backend.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableMethodSecurity
+class WebSecurityConfig {
+
+    @Bean
+    fun authenticationManager(authConfig: AuthenticationConfiguration): AuthenticationManager {
+        return authConfig.authenticationManager
+    }
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf {
+            it.disable()
+        }.headers {
+            it.frameOptions { frameOptions ->
+                frameOptions.disable()
+            }
+        }
+
+        return http.build()
+    }
+
+}
